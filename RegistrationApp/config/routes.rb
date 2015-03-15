ba892b99006f9e9ff2374eb5345552f8454ed4c4
Ulrika Falk <uf222ba@student.lnu.se>
2015-03-15 15:07:30 +0000

    Fixat till så att bootstrap fungerar. Lagt till vyer.

--- RegistrationApp/config/routes.rb
+++ RegistrationApp/config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/index' => 'users#index'
+  get '/new' => 'users#new'
+  
 end
